8139773c135649e5a4a917cca50bd737b024ecff
Darkryhr <gabyk800@gmail.com>
2022-01-21 19:38:56 +0200

    initial commit

---
+++ pages/about/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+const About = () => {
+  return (
+    <>
+      <main className='wrapper'>
+        <Header />
+        <h1>About</h1>
+      </main>
+    </>
+  );
+};
+
+export default About;
---
+++ pages/store/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+const Store = () => {
+  return (
+    <>
+      <main className='wrapper'>
+        <Header />
+        <h1>Store</h1>
+      </main>
+    </>
+  );
+};
+
+export default Store;
