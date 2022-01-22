dc0dbae5f7086bfc3adac6d02fd7dbab1dcbf07a
Darkryhr <gabyk800@gmail.com>
2022-01-22 10:48:27 +0200

    store grid

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 const About = () => {
   return (
     <>
+      <div className='nav-block'></div>
       <main className='wrapper'>
         <Header />
         <h1>About</h1>
--- pages/store/index.jsx
+++ pages/store/index.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import products from '../../products.json';
+import styles from '../../styles/Store.module.scss';
 
 const Store = () => {
   return (
     <>
+      <div className='nav-block'></div>
       <main className='wrapper'>
         <Header />
         <h1>Store</h1>
+        <ul className={styles.grid}>
+          {products.map((product) => {
+            const { id, title, image, description, price } = product;
+            return (
+              <li key={id} className={styles.card}>
+                <a href='#'>
+                  <img src={image} alt={title} />
+                  <div className={styles.content}>
+                    <div className={styles.heading}>
+                      <h3>{title}</h3>
+                      <p>${price}</p>
+                    </div>
+                    <p>{description}</p>
+                  </div>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </main>
     </>
   );
