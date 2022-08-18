c48988443136bc02bc7226b033813df3dcd84449
Gabriel Lellouche <gabyk800@gmail.com>
2022-08-18 20:42:56 +0300

    arrow overlap fix

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,7 @@ const Home = () => {
       <main className='max-w-screen-xl px-2 mx-auto'>
         <section className='md:w-1/2 px-8 h-[90vh] md:text-gray-700 text-white flex flex-col justify-center md:pb-24 sm:pb-48 pb-56 relative'>
           <div className='absolute'>
-            <h1 className='font-bold text-7xl sm:max-w-none max-w-[90px]'>
-              Rule the cold.
-            </h1>
+            <h1 className='font-bold text-7xl sm:max-w-none'>Rule the cold.</h1>
             <p className='text-lg md:max-w-none max-w-xs'>
               Clothes made to last. Look and feel your best in any situation.
             </p>
