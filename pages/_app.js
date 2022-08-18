008e4af308c0102cafc858d2ead4e34d43e8ed58
Gabriel Lellouche <gabyk800@gmail.com>
2022-08-18 17:57:29 +0300

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
 import '@styles/globals.css';
 import { CartProvider } from '@lib/useCart';
 import Layout from '@components/Layout';
+import { Head } from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
       <Layout>
+        <Head>
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff' />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </CartProvider>
